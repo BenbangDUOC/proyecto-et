cb87c82fe5a2f735662fa0922136247c3532f30b
Mapabaoy12 <mat.turra@duocuc.cl>
2026-06-29 17:26:22 -0400

    Pequenias correcciones pipeline, conexion a BD

--- dashboards/app.py
+++ dashboards/app.py
@@ -18,36 +18,40 @@ st.markdown("En este dashboard se presentan los resultados del modelo de segment
 
 #Función para obtener los datos del dashboard desde el servicio FastAPI
 def obtener_datos():
-    respuesta = requests.get("http://localhost:8000/dashboard-data")
+    respuesta = requests.get("http://ml-service:8000/dashboard-data")
     respuesta.raise_for_status()  # Lanza un error si la solicitud no fue exitosa
     payload = respuesta.json()
     clientes = pd.DataFrame(payload['clientes'])
     centroides = pd.DataFrame(payload['centroides'])
-    metricas = pd.DataFrame(payload['metricas'])
+    metricas = pd.DataFrame([payload['metricas']])
     return clientes, centroides, metricas
 
 #Ejecución de la función para obtener los datos
 clientes, centroides, metricas = obtener_datos()
 
 #Métricas del modelo y justificación de la segmentación
+
 st.subheader("Métricas del modelo")
 st.markdown("En esta sección se presentan las métricas de evaluación del modelo, permitiendo entender la calidad de la segmentación realizada")
 
 #Muestra de las métricas del modelo
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.metric('K óptimo', metricas['k_optimo'])
 with col2:
     st.metric('Silhouette Score', round(metricas['silhouette_score'],4))
 with col3:
     st.metric('Varianza PCA', round(metricas['varianza_pca'],4))
+with col4:
+    st.metric('Inercia', round(metricas['inercia'], 2)) # <--- Nueva métrica
 
 #Scatter plot de PCA con centroides
 st.subheader("Visualización de la segmentación de clientes con centroides")
+
 fig_scatter = px.scatter(
     clientes,
-    x='PCA1',
-    y='PCA2',
+    x='pc1',
+    y='pc2',
     color='cluster',
     title='Visualización PCA de la segmentación de clientes',
     labels={'PCA1': 'Componente Principal 1', 'PCA2': 'Componente Principal 2', 'cluster': 'Cluster'}
@@ -64,6 +68,7 @@ fig_scatter.update_layout(
     legend=dict(title='Cluster', x=1, y=1),
     margin=dict(l=0, r=0, t=30, b=0)
 )
+st.plotly_chart(fig_scatter)
 
 #Gráfico de barras para la cantidad de clientes por cluster
 st.subheader("Cantidad de clientes por cluster")
@@ -81,5 +86,7 @@ fig_bar.update_layout(
     barmode='group',
     margin=dict(l=0, r=0, t=30, b=0)
 )
-
+st.plotly_chart(fig_bar)
 #Falta por agregar: método del codo(gráfico), perfil de los clusters(heatmap), comparación de los centroides(radar chart), análisis de variables específicas por clúster (boxplot), análisis de la información
+st.write("Datos de métricas recibidos por Streamlit:")
+st.json(metricas.to_dict()) # Esto mostrará el JSON en la pantalla
