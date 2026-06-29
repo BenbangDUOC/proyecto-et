88dd0270695424a201b272682aefd2dc83c66b57
GustavoAlvial <gu.alvial@duocuc.cl>
2026-06-29 04:17:58 -0400

    Creación visualizaciones: Scatter plot PCA y graf. barras cantidad de clientes por cluster

--- dashboards/app.py
+++ dashboards/app.py
@@ -21,13 +21,13 @@ def obtener_datos():
     respuesta = requests.get("http://localhost:8000/dashboard-data")
     respuesta.raise_for_status()  # Lanza un error si la solicitud no fue exitosa
     payload = respuesta.json()
-    usuarios = pd.DataFrame(payload['usuarios'])
+    clientes = pd.DataFrame(payload['clientes'])
     centroides = pd.DataFrame(payload['centroides'])
     metricas = pd.DataFrame(payload['metricas'])
-    return usuarios, centroides, metricas
+    return clientes, centroides, metricas
 
 #Ejecución de la función para obtener los datos
-usuarios, centroides, metricas = obtener_datos()
+clientes, centroides, metricas = obtener_datos()
 
 #Métricas del modelo y justificación de la segmentación
 st.subheader("Métricas del modelo")
@@ -41,3 +41,45 @@ with col2:
     st.metric('Silhouette Score', round(metricas['silhouette_score'],4))
 with col3:
     st.metric('Varianza PCA', round(metricas['varianza_pca'],4))
+
+#Scatter plot de PCA con centroides
+st.subheader("Visualización de la segmentación de clientes con centroides")
+fig_scatter = px.scatter(
+    clientes,
+    x='PCA1',
+    y='PCA2',
+    color='cluster',
+    title='Visualización PCA de la segmentación de clientes',
+    labels={'PCA1': 'Componente Principal 1', 'PCA2': 'Componente Principal 2', 'cluster': 'Cluster'}
+)
+fig_scatter.add_trace(go.Scatter(
+    #x=centroides['PCA1'], falta guardar estas variables en el endpoint de FastAPI
+    #y=centroides['PCA2'],
+    mode='markers',
+    marker=dict(color='black', size=12, symbol='x'),
+    name='Centroides'
+))
+fig_scatter.update_traces(marker=dict(size=7), selector=dict(mode='markers'))
+fig_scatter.update_layout(
+    legend=dict(title='Cluster', x=1, y=1),
+    margin=dict(l=0, r=0, t=30, b=0)
+)
+
+#Gráfico de barras para la cantidad de clientes por cluster
+st.subheader("Cantidad de clientes por cluster")
+fig_bar = go.Figure()
+for cluster in clientes['cluster'].unique():
+    fig_bar.add_trace(go.Bar(
+        x=[cluster],
+        y=[len(clientes[clientes['cluster'] == cluster])],
+        name=f'Cluster {cluster}'
+    ))
+fig_bar.update_layout(
+    title='Cantidad de clientes por cluster',
+    xaxis_title='Cluster',
+    yaxis_title='Cantidad de clientes',
+    barmode='group',
+    margin=dict(l=0, r=0, t=30, b=0)
+)
+
+#Falta por agregar: método del codo(gráfico), perfil de los clusters(heatmap), comparación de los centroides(radar chart), análisis de variables específicas por clúster (boxplot), análisis de la información
